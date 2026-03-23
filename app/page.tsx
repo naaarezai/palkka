f8527bf6ceb46439bc0bbcbd930d595f024f25f1
naaarezai <nasratollah.rezai@gmail.com>
2026-03-23 22:08:41 +0200

    Poisto nappin korjaus 1.3.1

--- app/page.tsx
+++ app/page.tsx
@@ -1032,7 +1032,7 @@ export default function Home() {
                                  </div>
                                  <button 
                                   onClick={() => handleDeleteShift(shift.id)}
-                                  className="p-2 text-slate-500 hover:text-red-400 hover:bg-red-950/20 rounded-lg transition opacity-0 group-hover:opacity-100"
+                                  className="p-2 text-slate-500 hover:text-red-400 hover:bg-red-950/20 rounded-lg transition sm:opacity-0 sm:group-hover:opacity-100"
                                  >
                                    <Trash2 size={18} />
                                  </button>
